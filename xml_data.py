60cb3cb9e58fafbbfb04a20093ac1930a33098d1
kideveloper612 <developer61510@gmail.com>
2019-12-28 03:42:52 +0800

    file write

--- xml_data.py
+++ xml_data.py
@@ -14,10 +14,10 @@ en_caption = source.captions.get_by_language_code('en')
 # en_caption_convert_to_srt =(en_caption.generate_srt_captions())
 en_caption_convert_to_xml = (en_caption.xml_captions)
 print(en_caption_convert_to_xml)
-# pretty_xml_as_string = etree.tostring(en_caption_convert_to_xml, encoding='UTF-8', xml_declaration=True, pretty_print=True)
+pretty_xml_as_string = etree.tostring(en_caption_convert_to_xml, encoding='UTF-8', xml_declaration=True, pretty_print=True)
 
-# pretty_xml_as_string = xml.dom.minidom.parse(en_caption_convert_to_xml).toprettyxml()
-#
-# text_file = open("test.xml", "w")
-# text_file.write(pretty_xml_as_string)
-# text_file.close()
+pretty_xml_as_string = xml.dom.minidom.parse(en_caption_convert_to_xml).toprettyxml()
+
+text_file = open("test.xml", "w")
+text_file.write(pretty_xml_as_string)
+text_file.close()
