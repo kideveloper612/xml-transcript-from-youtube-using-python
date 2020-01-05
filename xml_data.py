8d4b03eb1bbc2c86604cb4afe17a46344f93ea73
kideveloper612 <developer61510@gmail.com>
2020-01-05 22:54:22 +0800

    space

--- xml_data.py
+++ xml_data.py
@@ -9,7 +9,7 @@ from lxml import etree
 source = YouTube('https://youtu.be/2ejMpDd3PL0')
 
 en_caption = source.captions.get_by_language_code('en')
-# en_caption_convert_to_srt =(en_caption.generate_srt_captions())
+en_caption_convert_to_srt =(en_caption.generate_srt_captions())
 en_caption_convert_to_xml = (en_caption.xml_captions)
 print(en_caption_convert_to_xml)
 pretty_xml_as_string = etree.tostring(en_caption_convert_to_xml, encoding='UTF-8', xml_declaration=True, pretty_print=True)
